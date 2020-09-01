5dec0bb032b95960719f4284d14f032c82add857
Eric Stone <stone.ericm@gmail.com>
2020-09-01 17:23:25 -0400

    wrote database creator

---
+++ database.py
@@ -0,0 +1,24 @@
+import sqlalchemy as alchemy
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, ForeignKeyConstraint, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine('sqlite:///app.db')
+
+metadata = MetaData()
+
+
+class Records(Base):
+    __tablename__ = 'records'
+
+    id = Column(Integer, primary_key=True)
+    year = Column(Integer)
+    team = Column(String)
+    wins = Column(Integer)
+    losses = Column(Integer)
+    org_founded = Column(Integer)
+    expansion = Column(Boolean)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
