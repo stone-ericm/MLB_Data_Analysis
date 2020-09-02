63eb2b026660393eff15d0a5bd3b90d2a15c2ddf
Eric Stone <stone.ericm@gmail.com>
2020-09-02 18:34:13 -0400

    moved models out of database initialization

--- init_database.py
+++ init_database.py
@@ -15,32 +15,7 @@ engine = create_engine('sqlite:///app.db')
 
 metadata = MetaData()
 
-
-class Records(Base):
-    __tablename__ = 'records'
-
-    id = Column(Integer, primary_key=True)
-    year = Column(Integer)
-    team_id = Column(String)
-    league = Column(String)
-    wins = Column(Integer)
-    losses = Column(Integer)
-    org_founded = Column(Integer)
-    expansion = Column(Boolean)
-
-
-# class Franchises(Base):
-#     __tablename__ = 'franchises'
-
-#     id = Column(Integer, primary_key=True)
-#     team_id = Column(String, ForeignKey('records.team_id'))
-
-
-class Alt_Names(Base):
-    __tablename__ = 'alt_names'
-
-    alternate_name = Column(String, primary_key=True)
-    team_id = Column(String, ForeignKey('records.team_id'))
+from models import *
 
 
 if __name__ == "__main__":
---
+++ models.py
@@ -0,0 +1,32 @@
+import sqlalchemy as alchemy
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, ForeignKeyConstraint, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Records(Base):
+    __tablename__ = 'records'
+
+    id = Column(Integer, primary_key=True)
+    year = Column(Integer)
+    team_id = Column(String)
+    league = Column(String)
+    wins = Column(Integer)
+    losses = Column(Integer)
+    org_founded = Column(Integer)
+    expansion = Column(Boolean)
+
+
+# class Franchises(Base):
+#     __tablename__ = 'franchises'
+
+#     id = Column(Integer, primary_key=True)
+#     team_id = Column(String, ForeignKey('records.team_id'))
+
+
+class Alt_Names(Base):
+    __tablename__ = 'alt_names'
+
+    alternate_name = Column(String, primary_key=True)
+    team_id = Column(String, ForeignKey('records.team_id'))
