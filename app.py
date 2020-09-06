0b318ee799d8e76e3e8f120ba017c7fc7065ebac
Eric Stone <stone.ericm@gmail.com>
2020-09-06 01:02:46 -0400

    Changes to database models to improve the relationship between tables. Added function to determine average win percentage for expansion/non-expansion teams

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 # from requests_html import HTMLSession
 import sqlalchemy as alchemy
 from sqlalchemy import MetaData, insert, create_engine, Table
+from sqlalchemy.orm import load_only
 from models import Records, Franchises
 from statistics import mean
 
@@ -10,89 +11,85 @@ engine = create_engine("sqlite:///app.db")
 # engine.echo = True
 Session = alchemy.orm.sessionmaker(bind=engine)
 alc_session = Session()
+records = alc_session.query(Records)
+franchises = alc_session.query(Franchises)
 
-'''
-Lets figure out the average season for each team
-and again maybe with some number of early years excluded as a buffer
-for expansion teams
 
-What's the average number of years before an expansion team has a .500+ record?
-'''
-team_ids = []
-team_records = {}
-franchise_objects = alc_session.query(Franchises).all()
-for each in franchise_objects:
-    team_ids.append(each.team_id)
-for team in team_ids:
-    print(team)
-    team_records[team] = alc_session.query(
-        Records).filter_by(team_id=team).all()
-# team_records in each loop contains the records for one respective team as an instance of Records
-    # years_in_operation = len(team_records)
-    # print(years_in_operation)
-len(team_records)
-print(team_records)
-collective_win_percentages = []
-for i, each in team_records:
-    for each in each:
+def grab_team_all_records(team):
+    return records.filter(Records.team_id).all()
+
+
+def grab_team_by_year(team, year):
+    return records.filter(Records.team_id == team, Records.year == year).all()
+
+
+def average_record_by_team(team):
+    annual_records = records.filter(Records.team_id == team).all()
+    collective_win_percentages = []
+    # print(annual_records)
+    for each in annual_records:
         win_percent = each.wins / (each.losses + each.wins)
-    # print(win_percent)
-    collective_win_percentages.append(win_percent)
-average_season = mean(collective_win_percentages)
-print("The average win percentage for {} is {:.3f}".format(team, average_season))
-
-# session = HTMLSession()
-# year = str(2020)
-# URL = "https://widgets.sports-reference.com/wg.fcgi?css=1&site=br&url=%2Fleagues%2FMLB%2F" + year + \
-#     "-standings.shtml&div=div_expanded_standings_overall&del_col=1,4,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30"
-# r = session.get(URL)
-# print(r)
-
-# soup = BeautifulSoup(r.content, 'html.parser')
-
-# table = soup.find(id="expanded_standings_overall")
-
-# # print(table.findAll("tr")[0])
-# # print(table.findAll("tr")[1]("td")[0].get_text())
-
-# # record_instance = Records(
-# #     year=int(year)
-# #     team=)
-
-# f = open("modern_names", "w")
+        collective_win_percentages.append(win_percent)
+    average_season = mean(collective_win_percentages)
+    return("The average win percentage for {} is {:.3f}".format(team, average_season))
 
 
-# counter = 0
-# # while counter < 2:
-# for row in table.findAll("tr"):
-#     # for cell in row("td"):
-#     #     print(cell.get_text())
-#     if row("td") != [] and row("td")[0].get_text() != "Avg":
-#         f.write(row("td")[0].get_text() + "\n")
-# print(row("td"))
-# new_record = Records(
-#     year=int(year),
-#     team=row("td")[0].get_text(),
-#     league=row("td")[1].get_text(),
-#     wins=row("td")[2].get_text(),
-#     losses=row("td")[3].get_text()
-# )
-# print(new_record.year)
-# print(new_record.team)
-# print(new_record.league)
-# print(new_record.wins)
-# print(new_record.losses)
-# print(new_record.org_founded)
-# break
+def years_until_500_season():
+    expansion_franchises = franchises.filter(
+        Franchises.expansion == True).options(load_only("team_id")).all()
+    fivehundred_plus_year = []
+    for team in expansion_franchises:
+        annual_records_exp_teams = records.filter(
+            Records.team_id == team.team_id).all()
+        for year in annual_records_exp_teams:
+            win_percent = year.wins / (year.losses + year.wins)
+            if win_percent >= .500:
+
+                fivehundred_plus_year.append(year.year - team.org_founded)
+                break
+    return(fivehundred_plus_year)
 
 
-# print(row("td"))
-# break
-# for cell in row("td"):
-# print(cell.get_text().strip())
+def years_until_consecutive_500_seasons():
+    expansion_franchises = franchises.filter(
+        Franchises.expansion == True).options(load_only("team_id")).all()
+    fivehundred_plus_year = {}
+    for team in expansion_franchises:
+        annual_records_exp_teams = records.filter(
+            Records.team_id == team.team_id).all()
+        flag = False
+        for year in annual_records_exp_teams:
+            win_percent = year.wins / (year.losses + year.wins)
+            if win_percent >= .500:
+                if flag == True:
+                    fivehundred_plus_year[team.team_id] = (
+                        year.year - team.org_founded)
+                    break
+                else:
+                    flag = True
+            else:
+                flag = False
+    return(fivehundred_plus_year)
 
 
-# f.close()
+def average_expansion_and_non_record():
+    annual_records = records.join(Franchises).all()
 
-# 2020_record = results.find_all('section', class_='')
-# print(results.prettify())
+    # print(annual_records[0].franchise.expansion)
+    # quit()
+    collective_win_percentages_exp = []
+    collective_win_percentages_non_exp = []
+    # print(annual_records)
+    for each in annual_records:
+        if each.year >= 1961:
+            win_percent = each.wins / (each.losses + each.wins)
+            if each.franchise.expansion == True:
+                collective_win_percentages_exp.append(win_percent)
+            else:
+                collective_win_percentages_non_exp.append(win_percent)
+    average_season_exp = mean(collective_win_percentages_exp)
+    average_season_non_exp = mean(collective_win_percentages_non_exp)
+    return("The average win percentage for a non-expansion team since 1961 is {:.3f}.\nThe average win percentage for an expansion team is {:.3f}.".format(average_season_non_exp, average_season_exp))
+
+
+print(average_expansion_and_non_record())
--- models.py
+++ models.py
@@ -1,8 +1,18 @@
 import sqlalchemy as alchemy
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, ForeignKeyConstraint, Boolean
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+class Franchises(Base):
+    __tablename__ = 'franchises'
+
+    # id = Column(Integer, primary_key=True)
+    team_id = Column(String, primary_key=True)
+    org_founded = Column(Integer)
+    expansion = Column(Boolean)
 
 
 class Records(Base):
@@ -10,19 +20,12 @@ class Records(Base):
 
     id = Column(Integer, primary_key=True)
     year = Column(Integer)
-    team_id = Column(String)
+    team_id = Column(String, ForeignKey("franchises.team_id"))
     league = Column(String)
     wins = Column(Integer)
     losses = Column(Integer)
-    org_founded = Column(Integer)
-    expansion = Column(Boolean)
-
-
-class Franchises(Base):
-    __tablename__ = 'franchises'
-
-    id = Column(Integer, primary_key=True)
-    team_id = Column(String, ForeignKey('records.team_id'))
+    franchise = relationship(Franchises)
+    # franchise_id = relationship("Franchises", back_populates="records")
 
 
 # class Alt_Names(Base):
